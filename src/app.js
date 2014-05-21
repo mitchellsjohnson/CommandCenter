f2e828795035bc1b0eada1d27383cbea4400c5c8
Richard Clement <richclement@gmail.com>
2014-05-21 11:35:11 -0400

    rearranging files and adding libraries

---
+++ src/app.js
@@ -0,0 +1,22 @@
+﻿//var http = require('http');
+//var port = process.env.port || 1337;
+
+var express = require('express');
+
+var app = express();
+
+
+//routes
+app.get('/', function(req, res) {
+    res.send('Please enter a channel <br\> Sample: http://commandcenter.evestment.com/channel/technology \n');
+});
+
+app.get('/channel/:name', function(req, res) {
+    
+    //TODO: add validation of channel name
+    var channelName = req.param.name;
+    
+
+});
+
+app.listen(3000);
