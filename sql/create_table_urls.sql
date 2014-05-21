fb40fe4580c6cb21419e01586754f2892ba65870
Mitchell Johnson <mitchelljohnson01@gmail.com>
2014-05-21 11:21:46 -0400

    Update to initial SQL tables and inserts
    creates initial tables, inserts Native URL type

--- sql/create_table_urls.sql
+++ sql/create_table_urls.sql
@@ -2,10 +2,14 @@ CREATE TABLE urls (
   url_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   url_string VARCHAR(2000) NOT NULL,
   description VARCHAR(200),
-  url_type int NOT NULL,
+  url_type_id int NOT NULL,
   active boolean NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT NOW(),
   created_by VARCHAR(100) NOT NULL,
   updated TIMESTAMP,
   updated_by VARCHAR(100)
 );
+
+
+ALTER TABLE urls ADD CONSTRAINT FK_url_url_type_id
+FOREIGN KEY (url_type_id) REFERENCES url_types(url_type_id)
